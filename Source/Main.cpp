94fb3745baa57fb5c1f2fba8f82c63547823de00
LmTinyToon <cool.ipatovo@gmail.com>
2016-07-16 20:21:53 +0300

    Rendering of model was added to main function

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -5,6 +5,7 @@
 //	Local includes
 #include "TGAImage.h"
 #include "Line.h"
+#include "Model.h"
 
 //	Constants/enums
 //	Width of image
@@ -16,7 +17,14 @@ int main(void)
 {
 	std::ofstream out_tga_file("TGATEST.tga", std::ios::binary);
 	TinyRender::TGAImage tga_image(width, height, TinyRender::TGAImage::ImageFormat_RGB);
-	TinyRender::render_line(0, 0, width, height, tga_image, TinyRender::TGAImage::Pixel(255, 0, 0));
+	TinyRender::Model model("../../Models/PyramidModel.txt");
+	TinyRender::Model::vertices_type& vertices = model.vertices();
+	for (size_t i = 0; i < vertices.size(); i += 9)
+	{
+		TinyRender::render_line(vertices[i], vertices[i + 1], vertices[i + 3], vertices[i + 4], tga_image, TinyRender::TGAImage::Pixel(255, 0, 0));
+		TinyRender::render_line(vertices[i + 3], vertices[i + 4], vertices[i + 6], vertices[i + 7], tga_image, TinyRender::TGAImage::Pixel(255, 0, 0));
+		TinyRender::render_line(vertices[i + 6], vertices[i + 7], vertices[i], vertices[i + 1], tga_image, TinyRender::TGAImage::Pixel(255, 0, 0));
+	}
 	tga_image << out_tga_file;
 	out_tga_file.close();
 	return 0;
