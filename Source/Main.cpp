1a3b5d83cfeabf80318a0998cda16c0402c9bfde
LmTinyToon <cool.ipatovo@gmail.com>
2016-07-16 19:33:03 +0300

    Rendering line into image was added (as simple example)

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -4,12 +4,19 @@
 
 //	Local includes
 #include "TGAImage.h"
+#include "Line.h"
+
+//	Constants/enums
+//	Width of image
+static const int width = 800;
+//	Height of image
+static const int height = 800;
 
 int main(void)
 {
 	std::ofstream out_tga_file("TGATEST.tga", std::ios::binary);
-	TinyRender::TGAImage tga_image(5, 5, TinyRender::TGAImage::ImageFormat_RGB);
-	tga_image.set_pixel(TinyRender::TGAImage::Pixel(255, 0, 0), 0, 0);
+	TinyRender::TGAImage tga_image(width, height, TinyRender::TGAImage::ImageFormat_RGB);
+	TinyRender::render_line(0, 0, width, height, tga_image, TinyRender::TGAImage::Pixel(255, 0, 0));
 	tga_image << out_tga_file;
 	out_tga_file.close();
 	return 0;
