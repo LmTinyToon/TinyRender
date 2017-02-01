a17cbeaad6e9771d13bde209b5c3d929ec096854
LmTinyToon <cool.ipatovo@gmail.com>
2017-02-01 19:04:35 +0300

    ObjParser plug file was created

--- Source/Model.cpp
+++ Source/Model.cpp
@@ -4,15 +4,83 @@
 //	Local includes
 #include "Model.h"
 
+//	Using namespaces
+using namespace std;
+
 //	TinyReader namespace
 namespace TinyRender
 {
 
+//	Classes
+class ObjParser
+{
+public:
+//	Constructors/destructors
+/*
+		ObjParser constructor
+		Params: file name
+*/
+	ObjParser(const string& file_name)
+	{
+		ifstream file_model(file_name);
+		parse_stream(file_model);
+	}
+
+protected:
+private:
+//	Methods
+/*
+		Parses ifstream file
+		Params: ifstream file
+		Return: none
+*/
+	void parse_stream(ifstream& file)
+	{
+		for (string buff; getline(file, buff); )
+		{
+			switch (buff[0])
+			{
+				case 'v': 
+					parse_vertex(buff);
+				break;
+				case 'f':
+					parse_face(buff);
+				break;
+				case '#':	
+					//	Skipping comment
+				break;
+			}
+		}
+	}
+
+/*
+		Parses vertex line
+		Params: vertex line
+		Return: none
+*/
+	void parse_vertex(const string& vertex_line)
+	{
+	}
+
+/*
+		Parses face line
+		Params: face line
+		Return: none
+*/
+	void parse_face(const string& face_line)
+	{
+		//	TODO: (alex) remove usage std exception class
+		throw exception("Parsing of faces is not implemented");
+	}
+
+//	Members
+};
+
 //	Model - constructors/destructor
-Model::Model(const std::string& model_name) : 
+Model::Model(const string& model_name) : 
 	m_vertices()
 {
-	std::ifstream file_model(model_name);
+	ifstream file_model(model_name);
 	size_t triangles_count = 0;
 	file_model >> triangles_count;
 	m_vertices.resize(triangles_count * 9);
