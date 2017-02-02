e9f1a9bbccf2ce941101ee2c5e53a83f0ec6bfa8
LAPTOP-5DP2Q63E\LmTinyToon <cool.ipatovo@gmail.com>
2017-02-02 20:59:59 +0300

    Initial version of lighting was added

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -16,19 +16,23 @@ static const int height = 800;
 
 int main(void)
 {
+	const TinyRender::Point light_vec(1.0, 0.0, 0.0, 0.0);
 	std::ofstream out_tga_file("TGATEST.tga", std::ios::binary);
 	TinyRender::TGAImage tga_image(width, height, TinyRender::TGAImage::ImageFormat_RGB);
 	TinyRender::Model model("../../Models/AfricanHead.obj");
 	for (size_t tr_id = 0; tr_id < model.triangles().size(); ++tr_id)
 	{
-		for (size_t i = 0; i < 3; ++i)
+		const TinyRender::Point& p1 = model.vertices()[model.triangles()[tr_id].vertices[0]];
+		const TinyRender::Point& p2 = model.vertices()[model.triangles()[tr_id].vertices[1]];
+		const TinyRender::Point& p3 = model.vertices()[model.triangles()[tr_id].vertices[2]];
+
+		TinyRender::Point norm_vec = (p2 - p1) ^ (p3 - p1);
+		norm_vec.normalize();
+		const float intense = light_vec * norm_vec;
+		if (intense > 0)
 		{
-			const TinyRender::Point& p1 = model.vertices()[model.triangles()[tr_id].vertices[i]];
-			const TinyRender::Point& p2 = model.vertices()[model.triangles()[tr_id].vertices[(i + 1) % 3]];
-			TinyRender::render_line((p1.x() + 1) * width / 2, 
-									(p1.y() + 1) * height / 2, 
-									(p2.x() + 1) * width / 2, 
-									(p2.y() + 1) * height / 2, tga_image, TinyRender::TGAImage::Pixel(255, 0, 0));
+			TinyRender::render_triangle(p1.x(), p1.y(), p2.x(), p2.y(), p3.x(), p3.y(), tga_image, 
+				TinyRender::TGAImage::Pixel(255 * intense, 255 * intense, 255 * intense));
 		}
 	}
 	tga_image << out_tga_file;
