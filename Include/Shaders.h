d1b60f15e71034d4e380865f2e84d3f026a7a4e7
LAPTOP-5DP2Q63E\LmTinyToon <cool.ipatovo@gmail.com>
2017-02-13 21:08:48 +0300

    Shaders.h/cpp files were created

---
+++ Include/Shaders.h
@@ -0,0 +1,23 @@
+//	Include guard
+#ifndef TINY_RENDER_SHADERS_INCLUDED_H__
+#define TINY_RENDER_SHADERS_INCLUDED_H__
+
+//	Std includes
+
+//	Includes
+
+//	TinyRender namespace 
+namespace TinyRender
+{
+
+//	Constants/enums
+
+//	Global classes
+
+//	Global functions
+
+//	TinyRender end namespace
+};
+
+//	Include guard end
+#endif
---
+++ Source/Shaders.cpp
@@ -0,0 +1,18 @@
+//	Std includes
+
+//	Local includes
+#include "Shaders.h"
+
+//	Using namespaces
+using namespace std;
+
+//	TinyReader namespace
+namespace TinyRender
+{
+
+//	Classes
+
+//	Global functions implementation
+
+//	TinyReader end namespace
+};
